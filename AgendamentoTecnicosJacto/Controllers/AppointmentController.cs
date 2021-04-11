b4cdaea95b7b76bb5bd907f83db32ea026e268a7
José Coque <jose.coque@hotmail.com>
2021-04-11 11:33:52 -0300

    feat: AppointmentCreateModelView & AppointmentController

---
+++ AgendamentoTecnicosJacto/Controllers/AppointmentController.cs
@@ -0,0 +1,26 @@
+﻿using AgendamentoTecnicosJacto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendamentoTecnicosJacto.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly IAppointment _appointmentService;
+        public AppointmentController(IAppointment employeeService)
+        {
+            _appointmentService = employeeService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new AppointmentCreateModelView();
+            return View(model);
+        }
+    }
+}
